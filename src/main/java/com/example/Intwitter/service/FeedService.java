f0154ba5a46b964d6da2dd7af74808d75650e38d
Kajal <kajal@rams-air.lan>
2018-10-15 17:49:13 -0700

    Clean up of warnings

--- src/main/java/com/example/Intwitter/service/FeedService.java
+++ src/main/java/com/example/Intwitter/service/FeedService.java
@@ -2,7 +2,6 @@ package com.example.Intwitter.service;
 
 import java.util.List;
 
-import com.example.Intwitter.entity.Intweet;
 import com.example.Intwitter.model.FeedResponse;
 
 public interface FeedService 
